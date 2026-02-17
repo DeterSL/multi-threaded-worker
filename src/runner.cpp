49d97fe5b9fd50e09ec96d425a487a0fa595cd63
Alexandre Iskandar <alexelgayar@gmail.com>
2026-02-17 16:38:17 +0000

    fix index counter bug

--- src/runner.cpp
+++ src/runner.cpp
@@ -14,9 +14,9 @@ namespace detersl::runner {
             acquired_cown_span<const detersl::types::Resource> ro_cown_arr,
             detersl::func::BasicFuncInfo basic_func) : func_info_(basic_func) {
         std::unordered_map<std::string, detersl::types::Resource*> local_resources;
+        size_t ro_index = 0;
+        size_t rw_index = 0;
         for (auto &res_name : func_info_.resources) {
-            size_t ro_index = 0;
-            size_t rw_index = 0;
             if(func_info_.read_only_resources.find(res_name) != func_info_.read_only_resources.end()){
                 // This is a read-only resource
                 local_resources[res_name] = const_cast<detersl::types::Resource*>(&(*ro_cown_arr.array[ro_index++]));
